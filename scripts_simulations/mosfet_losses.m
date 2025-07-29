e14bf4e7d54063f214337dce4c55f712e67cdcfa
Denis von Arx <dvarx@ethz.ch>
2025-07-29 14:48:21 +0200

    added LTSpice simulation files for ripple simulations

--- scripts_simulations/mosfet_losses.m
+++ scripts_simulations/mosfet_losses.m
@@ -8,7 +8,9 @@ Vgp=5.2;            %gate plateau voltage
 Cgd=30e-12;         %Cgd=Crss
 Vds=400;
 Ids=20;
-fsw=20e3;
+fsw=5e3;
+Qrr=5e-6;
+Rds=24e-3;
 
 t1=Rgon*Ciss*log(1/(1-Vth/VGS));
 t2=Rgon*Ciss*log(1/(1-Vgp/VGS));
@@ -16,10 +18,28 @@ t3=Rgon*Cgd*Vds/(VGS-Vgp);
 
 Eturnon=0.5*((t2-t1)+t3)*Vds*Ids;
 
+Err=Qrr*Vds*0.5;
+
 t4=Rgoff*Ciss*log(VGS/Vgp);
 t5=Rgoff*Cgd*Vds/Vgp;
 t6=Rgoff*Ciss*log(Vgp/Vth);
 
 Eturnoff=0.5*(t5+t6)*Vds*Ids;
 
-Pswitch=fsw*(Eturnon+Eturnoff);
+Psw=fsw*(Eturnon+Eturnoff+Err);
+Pcond=Rds*Ids^2;
+
+Ptot=Psw+Pcond;
+
+%electromagnet related parameters
+Rem=5;
+
+%ripple current through AC capacitor
+ils=linspace(0,20,10);
+ds=0.5+Rem*ils/(2*400);
+icdcs=(2*ds-1).*ils;
+icacs=sqrt(ils.^2-icdcs.^2);
+%capacitance value required
+T=1/fsw;
+alpha=4e-2;
+Cdc=T*Ids/(2*alpha*Vds);
